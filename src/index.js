2c5dbbc1c8756bff2d36b5b7c6556aa7a8a5691b
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-19 21:18:16 +0300

    Define a filter function to filter based on option selected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 console.log('%c HI', 'color: firebrick')
 
+function filterBreeds(firstCharacter) {
+    const breedsList = document.querySelectorAll('#dog-breeds li');
+
+    breedsList.forEach(breed => {
+        const dogBreed = breed.textContent;
+        // Check if the breed starts with the specified character
+        if (dogBreed.charAt(0) === firstCharacter) {
+            breed.style.display = ''; // Show the breed item
+        } else {
+            breed.style.display = 'none'; // Hide the breed item
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch dog images
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -20,7 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(breedUrl)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         const dogBreedsObj = data.message
         for(breed in dogBreedsObj){
             const dogBreed = document.createElement('li')
@@ -33,7 +46,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    // Filter
+    // Filter based on option selected
     const dogBreedDropdown = document.querySelector('#breed-dropdown')
-    
+    dogBreedDropdown.addEventListener('change', e => {
+        switch (e.target.value){
+            case 'a':
+                filterBreeds('a');
+                break;
+            case 'b':
+                filterBreeds('b');
+                break;
+            case 'c':
+                filterBreeds('c');
+                break;
+            case 'd':
+                filterBreeds('d');
+                break;
+        }
+    })
 })
